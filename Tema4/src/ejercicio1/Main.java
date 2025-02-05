c00800d24c8dc47981e2cbd559edbcff456230db
IZT612 <ivan.zamora@iesnervion.es>
2025-02-05 10:16:29 +0100

    Hecho 1 apartado C

--- Tema4/src/ejercicio1/Main.java
+++ Tema4/src/ejercicio1/Main.java
@@ -1,8 +1,17 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// Creamos el scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Variables enteras donde guardaremos las coordenadas a introducir
+		int coordenadaX;
+		int coordenadaY;
 		
 		// Creamos los objetos p1, p2 y p3, que son puntos, obtenidos de la clase Punto, dandole las coordenadas con las que iniciars
 		Punto p1 = new Punto(5, 0);
@@ -10,25 +19,29 @@ public class Main {
 		Punto p3 = new Punto(-3, 7);
 		
 		// Mostramos las coordenadas de los 3 puntos
-		System.out.println("Coordenadas del punto 1: (" + p1.coordenadaX + ", " + p1.coordenadaY + ")" );
-		System.out.println("Coordenadas del punto 2: (" + p2.coordenadaX + ", " + p2.coordenadaY + ")" );
-		System.out.println("Coordenadas del punto 3: (" + p3.coordenadaX + ", " + p3.coordenadaY + ")" );
+		System.out.println("Coordenadas del punto 1: (" + p1.getX() + ", " + p1.getY() + ")" );
+		System.out.println("Coordenadas del punto 2: (" + p2.getX() + ", " + p2.getY() + ")" );
+		System.out.println("Coordenadas del punto 3: (" + p3.getX() + ", " + p3.getY() + ")" );
 		System.out.println();
 		
-		// Probamos operadores básicos con las coordenadas
-		p1.coordenadaX += 2;
-		p1.coordenadaY -= 2;
+		// Volvemos a pedir las coordenadas del punto y las guardamos, para cambiar las coordenadas con las funciones "setX" y "setY"
+		System.out.println("Vamos a cambiar las coordenadas del punto 1");
+		System.out.println("Introduzca la nueva coordenada X");
+		coordenadaX = sc.nextInt();
+		p1.setX(coordenadaX);
+		System.out.println("Introduzca ahora la nueva coordenada Y");
+		coordenadaY = sc.nextInt();
+		p1.setY(coordenadaY);
 		
-		p2.coordenadaX *= 5;
-		p2.coordenadaY /= 5;
 		
-		p3.coordenadaX = (int) Math.sqrt(p3.coordenadaX);
-		p3.coordenadaY %= 2;
 		
-		// Mostramos las coordenadas de los 3 puntos
-		System.out.println("Coordenadas del punto 1: (" + p1.coordenadaX + ", " + p1.coordenadaY + ")" );
-		System.out.println("Coordenadas del punto 2: (" + p2.coordenadaX + ", " + p2.coordenadaY + ")" );
-		System.out.println("Coordenadas del punto 3: (" + p3.coordenadaX + ", " + p3.coordenadaY + ")" );
+		// Mostramos las coordenadas del punto 1
+		System.out.println("Coordenadas del punto 1: (" + p1.getX() + ", " + p1.getY() + ")" );
+		System.out.println("Coordenadas del punto 2: (" + p2.getX() + ", " + p2.getY() + ")" );
+		System.out.println("Coordenadas del punto 3: (" + p3.getX() + ", " + p3.getY() + ")" );
+	
+		// Cerramos el scaner
+		sc.close();
 		
 	}
 
--- Tema4/src/ejercicio1/Punto.java
+++ Tema4/src/ejercicio1/Punto.java
@@ -6,10 +6,46 @@ package ejercicio1;
 public class Punto {
 
 	// Creamos el atributo de la clase "Punto" coordenadaX
-	int coordenadaX;
+	private int coordenadaX = 0;
 	
 	// Creamos el atributo de la clase "Punto" coordenadaY
-	int coordenadaY;
+	private int coordenadaY = 0;
+	
+	// Función publica para acceder a la coordenada X
+	public int getX() {
+		
+		return coordenadaX;
+		
+	}
+	
+	// Función publica para acceder a la coordenada Y
+	public int getY() {
+		
+		return coordenadaY;
+		
+	}
+	
+	// Función publica para cambiar la coordenada X del objeto
+	public void setX(int coordenadaX) {
+		
+		if (coordenadaX > 0) {
+			
+			this.coordenadaX = coordenadaX;
+			
+		}
+		
+	}
+	
+	// Función publica para cambiar la coordenada Y del objeto
+	public void setY(int coordenadaY) {
+		
+		if (coordenadaY > 0) {
+			
+			this.coordenadaY = coordenadaY;
+			
+		}
+		
+	}
 	
 	/**
 	 * Constructor que obtiene dos coordenadas y las aplica al objeto
